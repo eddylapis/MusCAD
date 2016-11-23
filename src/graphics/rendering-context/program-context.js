6569b4098b420b7861647ee0f51f164417cc32f0
ryanzyy <ryanzyy@gmail.com>
2016-11-23 21:03:45 +1100

    Add uniforms to ProgramContext

--- src/graphics/rendering-context/program-context.js
+++ src/graphics/rendering-context/program-context.js
@@ -13,11 +13,13 @@ export default class ProgramContext {
     this._gl = gl;
     this._glProgram = glProgram;
     this._attributes = {};
+    this._uniforms = {};
   }
 
   get gl() { return this._gl; }
   get glProgram() { return this._glProgram; }
   get attributes() { return this._attributes; }
+  get uniforms() { return this._uniforms; }
 
   initialize(gl, glProgram, cb) {
     this._gl = gl;
@@ -42,5 +44,13 @@ export default class ProgramContext {
       stride * bytesForArray,
       offset * bytesForArray
     );
+  }
+
+  initUniform(uniformName) {
+    this.uniforms[uniformName] = this.gl.getUniformLocation(this.glProgram, uniformName);
+  }
+
+  uniformMat4(uniformName, matrix) {
+    this.gl.uniformMatrix4fv(this.uniforms[uniformName], false, matrix);
   }
 }
